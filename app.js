89b4d0e5d857d66840d1d8fd9bc5e069faab17ca
rfcho322 <98252280+rfcho322@users.noreply.github.com>
2023-03-15 03:29:58 +0800

    updated app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 
 app.get("/", function(req, res) {
     randomAdvice(res);
